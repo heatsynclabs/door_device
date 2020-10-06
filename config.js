564f9f01a806a2309df1ca562063826617335cbd
Luis Montes <monteslu@gmail.com>
2020-10-06 16:22:13 -0700

    initial

---
+++ config.js
@@ -0,0 +1,50 @@
+// Copyright 2019 Iced Development, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const Pack = require('./package');
+
+const env = process.env.NODE_ENV || 'development';
+
+// heatsync members_api
+const apiUrl = process.env.API_URL || 'http://localhost:3004';
+
+let mqttUrl = process.env.MQTT_URL;
+
+if (!mqttUrl) {
+  const url = new URL(apiUrl);
+  if (url.protocol === 'http:') {
+    mqttUrl = 'ws://' + url.host;
+  } else {
+    mqttUrl = 'wss://' + url.host;
+  }
+}
+
+// JWT to login with
+const jwt = process.env.MQTT_URL;
+
+// alternately device could login with username/password
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+const config = {
+  env,
+  apiUrl,
+  mqttUrl,
+  version: Pack.version,
+  jwt,
+  username,
+  password,
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,8 @@
+const rawr = require('rawr');
+const config = require('./config');
+
+async function run() {
+  console.log('hello world');
+}
+
+run();
